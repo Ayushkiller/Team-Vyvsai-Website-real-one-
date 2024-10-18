687e082964663ee152360586ef4d49dbe54eb830
Ayushkiller <152204318+Ayushkiller@users.noreply.github.com>
2024-10-18 23:55:43 +0530

    Update registerService.js

--- vyvsai-website/src/services/registerService.js
+++ vyvsai-website/src/services/registerService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const register = async (userData) => {
   try {
-    const response = await axios.post('http://localhost:5000/api/register', userData);
+    const response = await axios.post('https://panel.embotic.xyz/server/944a767c:5000/api/register', userData);
     return response.data;
   } catch (error) {
     throw error;
