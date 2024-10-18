8644fc186d5255ba830ba47554523c3ffe6f1f97
Ayushkiller <152204318+Ayushkiller@users.noreply.github.com>
2024-10-19 00:11:21 +0530

    Update registerService.js

--- vyvsai-website/src/services/registerService.js
+++ vyvsai-website/src/services/registerService.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-import https from 'https';
 
 const register = async (userData) => {
   try {
-    const response = await axios.post('https://38.69.12.10:1301/api/register', userData, {
-      httpsAgent: new https.Agent({ rejectUnauthorized: false })
-    });
+    const response = await axios.post('http://38.69.12.10:1301/api/register', userData);
     return response.data;
   } catch (error) {
     throw error;
