151793d45dadb23f2bdd4d0b75ec65e8abbc02b8
Ayushkiller <152204318+Ayushkiller@users.noreply.github.com>
2024-10-19 14:57:46 +0530

    nigga

---
+++ build/js/cardVisibility.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const heroSection = document.querySelector(".hero-section");
+  if (heroSection) {
+      heroSection.classList.add("visible");
+  }
+});
+
+document.addEventListener("scroll", function () {
+  const sections = document.querySelectorAll(
+      ".mission-section, .services-section, .values-section, .testimonials-section"
+  );
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+
+      if (sectionTop < triggerBottom) {
+          section.classList.add("visible");
+      }
+  });
+});
