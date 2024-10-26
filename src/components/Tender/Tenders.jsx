41bcda8dd42008d4f708c812995f58c18b1d895b
pooniaMayank <pooniamayank8@gmail.com>
2024-10-26 18:40:00 +0530

    removed colons from the tender form

--- src/components/Tender/Tenders.jsx
+++ src/components/Tender/Tenders.jsx
@@ -104,7 +104,7 @@ const Tenders = () => {
             <div className="form-group">
               <label htmlFor="state" className="form-label">
                 <i className="bi bi-map text-primary fs-5 me-2"></i>
-                State: <span className="text-danger">*</span>
+                State <span className="text-danger">*</span>
               </label>
               <select
                 id="state"
@@ -127,7 +127,7 @@ const Tenders = () => {
             <div className="form-group">
               <label htmlFor="district" className="form-label">
                 <i className="bi bi-geo-alt text-primary fs-5 me-2"></i>
-                District:
+                District
               </label>
               <select
                 id="district"
@@ -152,7 +152,7 @@ const Tenders = () => {
             <div className="form-group">
               <label htmlFor="department" className="form-label">
                 <i className="bi bi-building text-primary fs-5 me-2"></i>
-                Department:
+                Department
               </label>
               <select
                 id="department"
