850b20cec2c451b3c424385cbe8ab48c2505fb95
pooniaMayank <pooniamayank8@gmail.com>
2024-10-28 23:52:13 +0530

    form or jyada bhadda ho gya tha

--- src/components/Tender/Tenders.jsx
+++ src/components/Tender/Tenders.jsx
@@ -90,8 +90,8 @@ const Tenders = () => {
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100 w-75 meraPyaratenderForm">
-      <div className="form-container container p-1 shadow-lg rounded">
+    <div className="d-flex justify-content-center align-items-center min-vh-100 meraPyaratenderForm">
+      <div className="form-container container p-3 shadow-lg rounded">
         <h1 className="mb-4 text-center">Tender Search</h1>
         {loading && (
           <div className="loading-animation">
