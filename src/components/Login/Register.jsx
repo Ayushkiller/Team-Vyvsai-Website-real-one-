cc66eb27898579f1c14236f03c2f82c0a8a6e9e3
pooniaMayank <pooniamayank8@gmail.com>
2024-11-24 23:49:11 +0530

    removed bootstrap from component subscription

--- src/components/Login/Register.jsx
+++ src/components/Login/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const Register = () => {
   const [formData, setFormData] = useState({
